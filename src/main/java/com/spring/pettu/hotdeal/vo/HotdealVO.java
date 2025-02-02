f1060821fbfb8f13be5100c6eb9e83647228d7fd
sk-choi <victory9807@gmail.com>
2025-02-02 21:41:25 +0900

    [chore] 핫딜 vo 수정

--- src/main/java/com/spring/pettu/hotdeal/vo/HotdealVO.java
+++ src/main/java/com/spring/pettu/hotdeal/vo/HotdealVO.java
@@ -1,7 +1,4 @@
 package com.spring.pettu.hotdeal.vo;
-/*
-시퀀스: HOTDEAL_SEQ
-* */
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,15 +7,17 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class HotdealVO {
-    int proSeq;
-    String link;
-    String image;
-    String brand;
-    String maker;
-    String category1;
-    String category2;
-    String category3;
-    String category4;
-    int productId;
+public class HotdealVO { // DB에서 값을 가져오는 용도
+    private int proSeq;
+    private String title;
+    private String link;
+    private String image;
+    private String brand;
+    private String maker;
+    private String category1;
+    private String category2;
+    private String category3;
+    private String category4;
+    private String productId;
+    private int lowPrice;
 }
