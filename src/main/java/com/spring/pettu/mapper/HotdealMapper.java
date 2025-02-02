1a8e6655552a74721f3c2eb263519f191e056b96
sk-choi <victory9807@gmail.com>
2025-02-02 21:41:43 +0900

    [chore] 핫딜 매퍼 수정

--- src/main/java/com/spring/pettu/mapper/HotdealMapper.java
+++ src/main/java/com/spring/pettu/mapper/HotdealMapper.java
@@ -1,14 +1,19 @@
 package com.spring.pettu.mapper;
 
+import com.spring.pettu.hotdeal.vo.HotdealDTO;
 import com.spring.pettu.hotdeal.vo.HotdealVO;
-import com.spring.pettu.hotdeal.vo.HistoryVO;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.security.access.method.P;
 
 import java.util.List;
 
 public interface HotdealMapper {
 
-    public void insertHotdealData(@Param("hotdeal") HotdealVO hotdeal, @Param("history") HistoryVO history);
-    public List<HotdealVO> selectHotdealDataAll();
-    public HotdealVO selectHotdealDataById(@Param("proSeq") int proSeq);
+    public void insertHotdealData(@Param("hotdeal") HotdealDTO hotdeal);
+    public void insertHistoryData(@Param("hotdeal") HotdealDTO hotdeal, @Param("hotdealvo") HotdealVO hotdealvo);
+    public List<HotdealVO> selectAllHotdealData();
+    public HotdealVO selectByProductId(@Param("productId") String productId);
+    public int checkProductId(String productId);
+    public int checkRegitDate(@Param("hotdealvo") HotdealVO hotdealvo);
+    public void updateHistory(@Param("hotdeal") HotdealDTO hotdeal, @Param("hotdealvo") HotdealVO hotdealvo);
 }
