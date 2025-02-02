0fd573c6c3003c7cd608ea78a02998d3b46ebf37
sk-choi <victory9807@gmail.com>
2025-02-02 21:39:43 +0900

    [chore] ApiCallerService 수정

--- src/main/java/com/spring/pettu/hotdeal/service/ApiCallerService.java
+++ src/main/java/com/spring/pettu/hotdeal/service/ApiCallerService.java
@@ -1,11 +1,12 @@
 package com.spring.pettu.hotdeal.service;
 
-import com.spring.pettu.hotdeal.vo.HotdealVO;
+import com.spring.pettu.hotdeal.vo.HotdealDTO;
 import org.codehaus.jackson.JsonNode;
 
 import java.util.List;
 
 public interface ApiCallerService {
     public JsonNode getApiData(String query, int displayCount);
-    public List<HotdealVO> setHotdealData(JsonNode jsonNode); // 수정해야 함
+    public List<HotdealDTO> setHotdealData(JsonNode jsonNode);
+    public void saveHotdealToDB(List<HotdealDTO> hlist);// 수정해야 함
 }
