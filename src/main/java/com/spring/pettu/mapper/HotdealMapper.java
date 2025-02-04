7267d0cf82c7d306d305b7168397175a0292d46c
sk-choi <victory9807@gmail.com>
2025-02-04 15:42:09 +0900

    [chore] 핫딜 매퍼 메서드 추가

--- src/main/java/com/spring/pettu/mapper/HotdealMapper.java
+++ src/main/java/com/spring/pettu/mapper/HotdealMapper.java
@@ -1,7 +1,6 @@
 package com.spring.pettu.mapper;
 
-import com.spring.pettu.hotdeal.vo.HotdealDTO;
-import com.spring.pettu.hotdeal.vo.HotdealVO;
+import com.spring.pettu.hotdeal.vo.*;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.security.access.method.P;
 
@@ -13,7 +12,15 @@ public interface HotdealMapper {
     public void insertHistoryData(@Param("hotdeal") HotdealDTO hotdeal, @Param("hotdealvo") HotdealVO hotdealvo);
     public List<HotdealVO> selectAllHotdealData();
     public HotdealVO selectByProductId(@Param("productId") String productId);
+    public HotdealVO selectByProductSeq(@Param("proSeq") int proSeq);
     public int checkProductId(String productId);
     public int checkRegitDate(@Param("hotdealvo") HotdealVO hotdealvo);
     public void updateHistory(@Param("hotdeal") HotdealDTO hotdeal, @Param("hotdealvo") HotdealVO hotdealvo);
+    public List<HotdealJoinHistoryVO> findHotdeals(@Param("hotdealPagingVO") HotdealPagingVO hotdealPagingVO);
+    public List<HotdealJoinHistoryVO> selectSearch(@Param("hotdealPagingVO") HotdealPagingVO hotdealPagingVO, @Param("keyword") String keyword);
+    public int countHotdeals();
+    public int countSearch(@Param("keyword") String keyword);
+    public List<HistoryVO> selectHistoryByProSeq(@Param("proSeq") int proSeq);
+    public List<DiscountVO> selectDiscount();
+    public List<HotdealJoinHistoryVO> selectProductRecently();
 }
