bc75b9140b9fc76b4c28a0ea0f15aef51cef7b05
sk-choi <victory9807@gmail.com>
2025-02-04 15:38:25 +0900

    [chore] 핫딜 컨트롤러 수정

--- src/main/java/com/spring/pettu/hotdeal/controller/HotdealController.java
+++ src/main/java/com/spring/pettu/hotdeal/controller/HotdealController.java
@@ -1,15 +1,23 @@
 package com.spring.pettu.hotdeal.controller;
 
 import com.spring.pettu.hotdeal.service.HotdealServiceImpl;
+import com.spring.pettu.hotdeal.vo.HistoryVO;
+import com.spring.pettu.hotdeal.vo.HotdealPagingVO;
+import com.spring.pettu.hotdeal.vo.HotdealVO;
 import com.spring.pettu.review.controller.ReviewController;
 import com.spring.pettu.review.service.ReviewServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,14 +28,53 @@ public class HotdealController {
     private static final Logger logger = LoggerFactory.getLogger(HotdealController.class);
 
     @GetMapping("/home")
-    public String home(Model model) {
+    public String home(Model model, HotdealPagingVO hotdealPagingVO,
+                       @RequestParam(value="nowPage", required = false, defaultValue = "1") int nowPage,
+                       @RequestParam(value = "cntPerPage", required = false, defaultValue = "16") int cntPerPage) {
+
+        int total = hotdealService.getTotalHotdealCount();
+
+        hotdealPagingVO = new HotdealPagingVO(total, nowPage, cntPerPage);
+        System.out.println(hotdealService.getSearchDiscount());
+        model.addAttribute("hlist", hotdealService.getSearchHotdealRecently());
+        model.addAttribute("dlist", hotdealService.getSearchDiscount());
+        model.addAttribute("paging", hotdealPagingVO);
+        model.addAttribute("viewAll", hotdealService.getPagedHotdeals(hotdealPagingVO));
         model.addAttribute("contentPage", "/pettu/hotdeal/hotdeal_main.jsp");
         return "pettu/hotdeal/hotdeal_main";
     }
 
     @GetMapping("/detail")
-    public String detail(Model model) {
+    public String detail(Model model, @RequestParam(value="proSeq") int proSeq) {
+
+        model.addAttribute("hvo", hotdealService.getHotdealData(proSeq));
+        model.addAttribute("htlist", hotdealService.getHistoryByProSeq(proSeq));
         model.addAttribute("contentPage", "/pettu/hotdeal/hotdeal_detail.jsp");
         return "pettu/hotdeal/hotdeal_detail";
     }
+
+    @PostMapping("/chart")
+    public ResponseEntity<List<HistoryVO>> getHistoryByProSeq(@RequestParam(value="proSeq") int proSeq) {
+
+        List<HistoryVO> hlist = hotdealService.getHistoryByProSeq(proSeq);
+        return ResponseEntity.ok(hlist);
+    }
+
+    @GetMapping("/search")
+    public String search(Model model, HotdealPagingVO hotdealPagingVO,
+                                    @RequestParam(value="keyword") String keyword,
+                                    @RequestParam(value="nowPage", required = false, defaultValue = "1") int nowPage,
+                                    @RequestParam(value = "cntPerPage", required = false, defaultValue = "16") int cntPerPage)
+    {
+        int total = hotdealService.getCountSearchHotdeals(keyword);
+        hotdealPagingVO = new HotdealPagingVO(total, nowPage, cntPerPage);
+        model.addAttribute("paging", hotdealPagingVO);
+        model.addAttribute("slist", hotdealService.getSearchHotdeals(hotdealPagingVO, keyword));
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("contentPage", "/pettu/hotdeal/hotdeal_search.jsp");
+        return "pettu/hotdeal/hotdeal_search";
+    }
+
+
+
 }
