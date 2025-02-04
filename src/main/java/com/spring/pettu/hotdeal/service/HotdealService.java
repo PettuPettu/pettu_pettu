2b008a5f2e964ad38ef648c38b3927acc31342f8
sk-choi <victory9807@gmail.com>
2025-02-04 15:39:58 +0900

    [chore] 핫딜 서비스 수정

--- src/main/java/com/spring/pettu/hotdeal/service/HotdealService.java
+++ src/main/java/com/spring/pettu/hotdeal/service/HotdealService.java
@@ -1,5 +1,25 @@
 package com.spring.pettu.hotdeal.service;
 
+import com.spring.pettu.hotdeal.vo.*;
+
+import java.util.List;
+import java.util.Map;
+
 public interface HotdealService {
     public void hotdeal();
+
+    List<HotdealJoinHistoryVO> getPagedHotdeals(HotdealPagingVO hotdealPagingVO);
+    int getTotalHotdealCount();
+
+    List<HotdealJoinHistoryVO> getSearchHotdeals(HotdealPagingVO hotdealPagingVO, String search);
+    int getCountSearchHotdeals(String search);
+
+    HotdealVO getHotdealData(int proSeq);
+
+    List<HistoryVO> getHistoryByProSeq(int proSeq);
+
+    public List<Map<String, Object>> getSearchDiscount();
+
+    public List<Map<String, Object>> getSearchHotdealRecently();
 }
+
