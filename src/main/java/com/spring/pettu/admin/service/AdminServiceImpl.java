71e30b1dc1b7222326013885ef65507d8f958404
sk-choi <victory9807@gmail.com>
2025-02-05 14:34:34 +0900

    [chore] 어드민 서비스 impl import문 정리

--- src/main/java/com/spring/pettu/admin/service/AdminServiceImpl.java
+++ src/main/java/com/spring/pettu/admin/service/AdminServiceImpl.java
@@ -1,11 +1,9 @@
 package com.spring.pettu.admin.service;
 
 import com.spring.pettu.admin.vo.PagingVO;
-import com.spring.pettu.admin.vo.ReviewVO2;
 import com.spring.pettu.auth.user.vo.UserVO;
 import com.spring.pettu.mapper.AdminMapper;
 import com.spring.pettu.mypage.vo.PetVO;
-import com.spring.pettu.review.vo.ReviewVO;
 import com.spring.pettu.spot.vo.SpotVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
