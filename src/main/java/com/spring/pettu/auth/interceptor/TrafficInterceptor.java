aee685b6616c07ba4a12188fd15d37c02fbbf7aa
Yeongbee <handfaste@naver.com>
2025-02-07 13:51:45 +0900

    [FIX] 인터셉터 ban ip 수정

--- src/main/java/com/spring/pettu/auth/interceptor/TrafficInterceptor.java
+++ src/main/java/com/spring/pettu/auth/interceptor/TrafficInterceptor.java
@@ -32,9 +32,7 @@ public class TrafficInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String ip = request.getRemoteAddr();
         log.info("IP = {}, Url  = {}", ip, request.getRequestURI());
-        if (this.trafficLogService.getBanIp(ip) != null) {
-            throw new NoHandlerFoundException(request.getMethod(), request.getRequestURI(), new HttpHeaders());
-        } else {
+
             try {
                 if(!ip.equals("0:0:0:0:0:0:0:1") && !ip.equals("127.0.0.1")){
                     TrafficLogVO trafficLogVO = new TrafficLogVO();
@@ -49,6 +47,6 @@ public class TrafficInterceptor extends HandlerInterceptorAdapter {
             }
 
             return true;
-        }
+
     }
 }
