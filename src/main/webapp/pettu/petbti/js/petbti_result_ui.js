a538aea05e547208c912eb2009d7d8ba96f98cc5
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2025-02-07 21:23:11 +0900

    [fix] 결과 페이지에 로딩 시 빈문자열 방지를 위한 수정

--- src/main/webapp/pettu/petbti/js/petbti_result_ui.js
+++ src/main/webapp/pettu/petbti/js/petbti_result_ui.js
@@ -9,17 +9,12 @@ if (petName) {
     document.addEventListener("DOMContentLoaded", function () {
 
         const titlePetbti = document.querySelector(".petbti-title h1");
-        const descriptionPetbti = document.querySelector(".petbti-result-explanation strong");
-        const descriptionPetName = document.querySelector(".petbti-result-explanation b");
-
         const subTitle = document.querySelector(".petbti-result-subtitle");
         const tag = document.querySelector(".petbti-result-tag");
         const explanation = document.querySelector(".petbti-result-explanation");
         const img = document.querySelector(".petbti-result-img");
 
         titlePetbti.textContent = petMbti;
-        descriptionPetbti.textContent = petMbti;
-        descriptionPetName.textContent = petName;
         subTitle.textContent = petbtiObject.subTitle;
         img.src = petbtiObject.img;
 
@@ -29,6 +24,8 @@ if (petName) {
         })
         tag.textContent = tagStr;
 
+        const explanationStart = `<strong>${petMbti}</strong> 성향을 가진 <b>${petName}</b>(는)은<br/>`;
+        explanation.insertAdjacentHTML("beforeend",explanationStart );
         explanation.insertAdjacentHTML("beforeend",petbtiObject.explanation );
 
         sessionStorage.removeItem("petMbti");
