d9155d30f87c1c61516911450b8b7056dce63c7e
Yeongbee <handfaste@naver.com>
2025-02-07 11:48:54 +0900

    [FIX] 인터셉터 ip 수정

--- src/main/java/com/spring/pettu/auth/interceptor/TrafficInterceptor.java
+++ src/main/java/com/spring/pettu/auth/interceptor/TrafficInterceptor.java
@@ -1,35 +1,54 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.spring.pettu.auth.interceptor;
 
 import com.spring.pettu.auth.traffic.service.TrafficLogService;
 import com.spring.pettu.auth.traffic.vo.TrafficLogVO;
-import com.spring.pettu.mapper.TrafficMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.Generated;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+
 
 @Slf4j
+@RequiredArgsConstructor
 public class TrafficInterceptor extends HandlerInterceptorAdapter {
+
 
     @Autowired
     TrafficLogService trafficLogService;
 
-
-    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String ip = request.getRemoteAddr();
+        log.info("IP = {}, Url  = {}", ip, request.getRequestURI());
+        if (this.trafficLogService.getBanIp(ip) != null) {
+            throw new NoHandlerFoundException(request.getMethod(), request.getRequestURI(), new HttpHeaders());
+        } else {
+            try {
+                if(!ip.equals("0:0:0:0:0:0:0:1") && !ip.equals("127.0.0.1")){
+                    TrafficLogVO trafficLogVO = new TrafficLogVO();
+                    trafficLogVO.setClientIp(request.getRemoteAddr());
+                    trafficLogVO.setUserAgent(request.getHeader("User-Agent"));
+                    trafficLogVO.setRequestUri(request.getRequestURI());
+                    trafficLogService.save(trafficLogVO);
+                }
 
-        try {
-            TrafficLogVO trafficLogVO = new TrafficLogVO();
-            trafficLogVO.setClientIp(request.getRemoteAddr());
-            trafficLogVO.setUserAgent(request.getHeader("User-Agent"));
-            trafficLogVO.setRequestUri((String)request.getRequestURI());
-            trafficLogService.save(trafficLogVO);
-        } catch (Exception e) {
-            log.error("Traffic logging failed: " + e.getMessage(), e);
+            } catch (Exception e) {
+                log.error("Traffic logging failed: " + e.getMessage(), e);
+            }
+
+            return true;
         }
-        return true;
     }
 }
