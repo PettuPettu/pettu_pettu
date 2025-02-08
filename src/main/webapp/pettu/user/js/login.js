6a6619b439468c0e07a165f2127491ee0aa388ca
Yeongbee <handfaste@naver.com>
2025-02-08 14:40:52 +0900

    [Fix] 로그인 버튼 클릭시 1초간 비활성화

--- src/main/webapp/pettu/user/js/login.js
+++ src/main/webapp/pettu/user/js/login.js
@@ -2,6 +2,15 @@ $(document).ready(function () {
     $("form").submit(function (e) {
         e.preventDefault();
 
+        // 현재 폼 내의 제출 버튼을 찾아 disable
+        const $loginButton = $(this).find(":submit");
+        $loginButton.prop("disabled", true);
+
+        // 1초 후에 버튼을 다시 enable
+        setTimeout(function () {
+            $loginButton.prop("disabled", false);
+        }, 1000);
+
         // URL에서 redirectURL 파라미터 가져오기
         const urlParams = new URLSearchParams(window.location.search);
         const redirectURL = urlParams.get('redirectURL');
@@ -15,7 +24,7 @@ $(document).ready(function () {
                 redirectURL: redirectURL
             },
             success: function (response) {
-                // response 객체에서 redirectURL 확인
+                // response 객체에서 redirectURL 확인 후 이동
                 if (response.redirectURL) {
                     window.location.href = response.redirectURL;
                 } else {
@@ -23,18 +32,16 @@ $(document).ready(function () {
                 }
             },
             error: function (xhr) {
-                var errorMessage = "아이디 또는 비밀번호가 오류 입니다."; // 기본 메시지
+                var errorMessage = "아이디 또는 비밀번호가 오류 입니다.";
                 var response = xhr.responseText.trim();
                 if (response === "errorcode_1") {
                     errorMessage = "휴먼회원 입니다. 비밀번호 찾기 및 변경 기능을 통해 해제할 수 있습니다.";
                 }
-                // 에러 메시지를 표시할 div가 없으면 생성
                 if (!$('.error-message').length) {
                     $('input[name="password"]').after(
                         '<div class="error-message" style="color: red; font-size: 12px; margin-top: 5px;">' + errorMessage + '</div>'
                     );
                 } else {
-                    // 이미 있다면 메시지만 업데이트 (여기서 errorMessage 사용)
                     $('.error-message').text(errorMessage);
                 }
             }
