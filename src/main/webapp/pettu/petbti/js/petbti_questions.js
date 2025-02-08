93d5a607eeaf249fb32dbc13d7288c095eb9c91a
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2025-02-08 15:19:18 +0900

    [feat] 이름을 입력하지 않은 채 petbti 질문페이지에 접속할 경우 초기 페이지로 이동하는 로직 추가

--- src/main/webapp/pettu/petbti/js/petbti_questions.js
+++ src/main/webapp/pettu/petbti/js/petbti_questions.js
@@ -1,83 +1,90 @@
 import { petbtiQuestions } from './petbti_data.js';
 
-document.addEventListener("DOMContentLoaded", function () {
-    const mbtiScores = { E: 0, I: 0, S: 0, N: 0, T: 0, F: 0, J: 0, P: 0 };
+const petName = sessionStorage.getItem("petName");
 
-    let currentQuestionIndex = 0;
+if (petName) {
+    document.addEventListener("DOMContentLoaded", function () {
+        const mbtiScores = { E: 0, I: 0, S: 0, N: 0, T: 0, F: 0, J: 0, P: 0 };
 
-    const questionTitle = document.querySelector(".petbti-title h1");
-    const buttonsContainer = document.querySelector(".petbti-question-btn");
-    const progressbar = document.querySelector(".petbti-progressbar");
+        let currentQuestionIndex = 0;
 
-    function updatePetMbti(mbtiType, petSeq) {
-        const requestData = {
-            petMbti: mbtiType,
-            petSeq: petSeq,
-        };
+        const questionTitle = document.querySelector(".petbti-title h1");
+        const buttonsContainer = document.querySelector(".petbti-question-btn");
+        const progressbar = document.querySelector(".petbti-progressbar");
 
-        fetch('/petbti/mbti', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestData)
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('반려동물 MBTI 업데이트 성공:', data);
+        function updatePetMbti(mbtiType, petSeq) {
+            const requestData = {
+                petMbti: mbtiType,
+                petSeq: petSeq,
+            };
+
+            fetch('/petbti/mbti', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestData)
             })
-            .catch(error => {
-                console.error('반려동물 MBTI 업데이트 실패:', error);
+                .then(response => response.json())
+                .then(data => {
+                    console.log('반려동물 MBTI 업데이트 성공:', data);
+                })
+                .catch(error => {
+                    console.error('반려동물 MBTI 업데이트 실패:', error);
+                });
+        }
+
+        function updateQuestion(questions) {
+            if (currentQuestionIndex >= questions.length) {
+                showResult();
+                return;
+            }
+
+            const questionData = questions[currentQuestionIndex];
+            questionTitle.textContent = questionData.question;
+
+            buttonsContainer.innerHTML = "";
+            questionData.options.forEach((option) => {
+                const button = document.createElement("button");
+                button.textContent = option.text;
+                button.classList.add("petbti_btn");
+                button.addEventListener("click", () => handleAnswer(option.id, questions));
+                buttonsContainer.appendChild(button);
             });
-    }
-
-    function updateQuestion(questions) {
-        if (currentQuestionIndex >= questions.length) {
-            showResult();
-            return;
         }
 
-        const questionData = questions[currentQuestionIndex];
-        questionTitle.textContent = questionData.question;
+        function handleAnswer(optionId, questions) {
+            mbtiScores[optionId]++;
 
-        buttonsContainer.innerHTML = "";
-        questionData.options.forEach((option) => {
-            const button = document.createElement("button");
-            button.textContent = option.text;
-            button.classList.add("petbti_btn");
-            button.addEventListener("click", () => handleAnswer(option.id, questions));
-            buttonsContainer.appendChild(button);
-        });
-    }
+            if((currentQuestionIndex/petbtiQuestions.length)*100 <= 2) {
+                progressbar.style.width = "4%";
+            } else {
+                progressbar.style.width = `${(currentQuestionIndex/petbtiQuestions.length)*100}%`;
+            }
 
-    function handleAnswer(optionId, questions) {
-        mbtiScores[optionId]++;
-
-        if((currentQuestionIndex/petbtiQuestions.length)*100 <= 2) {
-            progressbar.style.width = "4%";
-        } else {
-            progressbar.style.width = `${(currentQuestionIndex/petbtiQuestions.length)*100}%`;
+            currentQuestionIndex++;
+            updateQuestion(questions);
         }
 
-        currentQuestionIndex++;
-        updateQuestion(questions);
-    }
+        function showResult() {
+            let mbtiType = "";
+            mbtiType += mbtiScores["E"] >= mbtiScores["I"] ? "E" : "I";
+            mbtiType += mbtiScores["S"] >= mbtiScores["N"] ? "S" : "N";
+            mbtiType += mbtiScores["T"] >= mbtiScores["F"] ? "T" : "F";
+            mbtiType += mbtiScores["J"] >= mbtiScores["P"] ? "J" : "P";
 
-    function showResult() {
-        let mbtiType = "";
-        mbtiType += mbtiScores["E"] >= mbtiScores["I"] ? "E" : "I";
-        mbtiType += mbtiScores["S"] >= mbtiScores["N"] ? "S" : "N";
-        mbtiType += mbtiScores["T"] >= mbtiScores["F"] ? "T" : "F";
-        mbtiType += mbtiScores["J"] >= mbtiScores["P"] ? "J" : "P";
+            sessionStorage.setItem("petMbti", mbtiType);
 
-        sessionStorage.setItem("petMbti", mbtiType);
+            if(sessionStorage.getItem("pSeq")!=null) {
+                updatePetMbti(mbtiType, sessionStorage.getItem("pSeq"));
+            }
 
-        if(sessionStorage.getItem("pSeq")!=null) {
-            updatePetMbti(mbtiType, sessionStorage.getItem("pSeq"));
+            window.location.href = "/petbti/result";
         }
 
-        window.location.href = "/petbti/result";
-    }
-
-    updateQuestion(petbtiQuestions);
-});
+        updateQuestion(petbtiQuestions);
+    });
+} else {
+    alert("잘못된 접근입니다. 시작 페이지로 이동합니다.");
+    window.location.href = "/petbti";
+}
